b9061638da935491dd260d87248ee0a1e8ee761e
Michael <michael.bailey788@topper.wku.edu>
2017-12-05 20:31:10 -0600

    added comments and made some functions private

--- genPuzzle.py
+++ genPuzzle.py
@@ -28,17 +28,20 @@ class Puzzle:
                     sub_mat = []
                     for subR in range(3):
                         for subC in range(3):
-                            sub_mat.append(sudoku_puzzle[sub_row * 3 + subR][sub_col * 3 + subC])
+                            sub_mat.append(
+                                sudoku_puzzle[sub_row * 3 + subR][sub_col * 3 + subC])
                     rand_val = 0
                     count = 0
                     while rand_val in this_row or rand_val in this_col or rand_val in sub_mat:
                         rand_val = random.randint(1, 9)
                         count += 1
 
-                        if count > max_attempts: break
+                        if count > max_attempts:
+                            break
                     sudoku_puzzle[row][col] = rand_val
 
-                    if count > max_attempts: break
+                    if count > max_attempts:
+                        break
                 if count > max_attempts:
                     break
 
@@ -48,8 +51,6 @@ class Puzzle:
         self.board = value
 
     #function that sets the value of a cell in a board
-    #input: self, 1D index, 2D index, number to assign
-    #output: none
     def set_value(self, i, j, num):
         self.board[i][j] = num
 
@@ -106,47 +107,53 @@ class Puzzle:
     #output: boolean
     def verify(self):
         #   check if numbers are between 1 and 9 (inclusive)
-        for row in self.board: # collect each row
-            for num in row: # collect each value in the row
-                if num not in range(1, 10): # if the value is not between 1-9, return false
+        for row in self.board:  # collect each row
+            for num in row:  # collect each value in the row
+                # if the value is not between 1-9, return false
+                if num not in range(1, 10):
                     return False
-        
+
         #   check rows for duplicates
-        for row in self.board: 
+        for row in self.board:
             #print row          not sure if this is needed anymore. uncomment if it is still needed.
             #print len(set(row))
-            if len(set(row)) != 9: # if the set (which only contains unique numbers) is not of length 9 (there was a duplicate number between 1-9), return false
+            if len(set(row)) != 9:  # if the set (which only contains unique numbers) is not of length 9 (there was a duplicate number between 1-9), return false
                 return False
 
         #   check columns for duplicates
         for i in range(0, len(self.board), 1):
             col = []
             for j in range(0, len(self.board[i]), 1):
-                col.append(self.board[j][i]) # append values of column into array, convert to set, and check set length
+                # append values of column into array, convert to set, and check set length
+                col.append(self.board[j][i])
             if len(set(col)) != 9:
                 return False
 
         #   check sub-matrix for duplicates
         for i in range(1, 8, 3):
             for j in range(1, 8, 3):
-                sub_matrix = [self.board[i - 1][j - 1], self.board[i][j - 1], self.board[i + 1][j - 1], 
-                              self.board[i - 1][j],     self.board[i][j],     self.board[i + 1][j], 
-                              self.board[i - 1][j + 1], self.board[i][j + 1], self.board[i + 1][j + 1]] # create submatrix as 1-dim array, convert to set, and check length
+                sub_matrix = [self.board[i - 1][j - 1], self.board[i][j - 1], self.board[i + 1][j - 1],
+                              self.board[i - 1][j],     self.board[i][j],     self.board[i + 1][j],
+                              self.board[i - 1][j + 1], self.board[i][j + 1], self.board[i + 1][j + 1]]  # create submatrix as 1-dim array, convert to set, and check length
                 if len(set(sub_matrix)) != 9:
                     return False
 
-        return True # at this point, if all tests pass, it is a valid sudoku board
-    
-    def check_row(self, rowIndex, num):
+        return True  # at this point, if all tests pass, it is a valid sudoku board
+
+    #function that checks a row for a number
+    def __check_row(self, rowIndex, num):
         return num in self.board[rowIndex]
 
-    def check_col(self, colIndex, num):
+    #function that checks a column for a number
+    def __check_col(self, colIndex, num):
         col = list()
         for j in range(len(self.board[colIndex])):
-            col.append(self.board[j][colIndex])
+            col.append(self.board[j][colIndex])  # collect column numbers
         return num in col
 
-    def check_mat(self, i, j, num):
+    #function that checks a submatrix for a number
+    def __check_mat(self, i, j, num):
+        #find center of submatrix given by row and col values
         if i in range(0, 3):
             i = 1
         if i in range(3, 6):
@@ -159,27 +166,39 @@ class Puzzle:
             j = 4
         if j in range(6, 9):
             j = 7
-        
-        subMatrix = [self.board[i - 1][j - 1], self.board[i][j - 1], self.board[i + 1][j - 1], self.board[i - 1][j], self.board[i][j], self.board[i + 1][j], self.board[i - 1][j + 1], self.board[i][j + 1], self.board[i + 1][j + 1]]
+
+        #collect submatrix
+        subMatrix = [self.board[i - 1][j - 1], self.board[i][j - 1], self.board[i + 1][j - 1], self.board[i - 1][j],
+                     self.board[i][j], self.board[i + 1][j], self.board[i - 1][j + 1], self.board[i][j + 1], self.board[i + 1][j + 1]]
         return num in subMatrix
 
-    def empty(self):
+    #function that checks if a slot is "empty"
+    def __empty(self):
         for i in range(len(self.board)):
             for j in range(len(self.board[i])):
                 if self.board[i][j] == 0:
-                    return i, j
-        return False, False
+                    return i, j  # return row and col value if empty
+        return False, False  # if no empty slots, then return boolean values
 
+    # function which uses backtracking to solve an incomplete board
+    # input: none
+    # output: boolean
     def solve(self):
-        row, col = 0, 0
-        row, col = self.empty()
-        if isinstance(row, bool):
-            return True
+        row, col = 0, 0  # set row and col to 0
+        row, col = self.__empty()  # gather row and column index of empty slot
+        if isinstance(row, bool):  # if returned value is boolean (no slot was found)
+            return True  # return true, which loops back in recursion
 
+        # go from one to nine
         for n in range(1, 10):
-            if not self.check_row(row, n) and not self.check_col(col, n) and not self.check_mat(row, col, n):
+            # if it follows sudoku rules
+            if not self.__check_row(row, n) and not self.__check_col(col, n) and not self.__check_mat(row, col, n):
+                # set the value of the slot to the proposed num
                 self.set_value(row, col, n)
+                #recursively go in, and if it is solved, return true
                 if self.solve():
                     return True
+                #if it doesn't work out, go back and set to 0 to try and find new value
                 self.set_value(row, col, 0)
+        # if all else fails, return false
         return False
